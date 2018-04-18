74910a43247930396b7f9dca431a38901802edc8
zhenghaven <hazheng@mix.wvu.edu>
2018-04-17 23:32:46 -0700

    Fixed the build process in sgxssl.cmake

--- libs/Intel_SGX/sgxssl.cmake
+++ libs/Intel_SGX/sgxssl.cmake
@@ -1,8 +1,11 @@
 cmake_minimum_required(VERSION 3.0)
 
+set(OPENSSL_SOURCE_SHA256_EXPECTED "5835626cde9e99656585fc7aaa2302a73a7e1340bf8c14fd635a62c66802a517")
+set(OPENSSL_SOURCE_VER 1.1.0h)
+set(SGX_SSL_BINARY_VER 1.9.100.0)
+
 get_filename_component(SSL_PATH "${CMAKE_CURRENT_LIST_DIR}/intel-sgx-ssl" ABSOLUTE)
 get_filename_component(SSL_BIN_PATH "${CMAKE_CURRENT_BINARY_DIR}/intel-sgx-ssl" ABSOLUTE)
-set(OPENSSL_SOURCE_SHA256_EXPECTED "5835626cde9e99656585fc7aaa2302a73a7e1340bf8c14fd635a62c66802a517")
 
 if(WIN32)
 	get_filename_component(SSL_WORK_DIR "${SSL_PATH}/Windows" ABSOLUTE)
@@ -10,22 +13,22 @@ else()
 	get_filename_component(SSL_WORK_DIR "${SSL_PATH}/Linux" ABSOLUTE)
 endif()
 
-get_filename_component(SSL_BUILD_DIR "${SSL_BIN_PATH}/build" ABSOLUTE)
-get_filename_component(SSL_ARCHIVE_PATH "${SSL_WORK_DIR}/sgxssl.1.9.100.0.zip" ABSOLUTE)
+set(SSL_BUILD_DIR "${SSL_BIN_PATH}/build")
+get_filename_component(SSL_ARCHIVE_PATH "${SSL_WORK_DIR}/sgxssl.${SGX_SSL_BINARY_VER}.zip" ABSOLUTE)
 
-if(EXISTS "${SSL_PATH}/openssl_source/openssl-1.1.0h.tar.gz")
-	file(SHA256 ${SSL_PATH}/openssl_source/openssl-1.1.0h.tar.gz OPENSSL_SOURCE_SHA256)
+if(EXISTS "${SSL_PATH}/openssl_source/openssl-${OPENSSL_SOURCE_VER}.tar.gz")
+	file(SHA256 ${SSL_PATH}/openssl_source/openssl-${OPENSSL_SOURCE_VER}.tar.gz OPENSSL_SOURCE_SHA256)
 endif()
 
-if(NOT EXISTS "${SSL_PATH}/openssl_source/openssl-1.1.0h.tar.gz" OR NOT ${OPENSSL_SOURCE_SHA256} STREQUAL ${OPENSSL_SOURCE_SHA256_EXPECTED})
+if(NOT EXISTS "${SSL_PATH}/openssl_source/openssl-${OPENSSL_SOURCE_VER}.tar.gz" OR NOT ${OPENSSL_SOURCE_SHA256} STREQUAL ${OPENSSL_SOURCE_SHA256_EXPECTED})
 	file(DOWNLOAD 
-		https://www.openssl.org/source/openssl-1.1.0h.tar.gz 
-		${SSL_PATH}/openssl_source/openssl-1.1.0h.tar.gz 
+		https://www.openssl.org/source/openssl-${OPENSSL_SOURCE_VER}.tar.gz 
+		${SSL_PATH}/openssl_source/openssl-${OPENSSL_SOURCE_VER}.tar.gz 
 		SHOW_PROGRESS
 		)
 endif()
 
-file(SHA256 ${SSL_PATH}/openssl_source/openssl-1.1.0h.tar.gz OPENSSL_SOURCE_SHA256)
+file(SHA256 ${SSL_PATH}/openssl_source/openssl-${OPENSSL_SOURCE_VER}.tar.gz OPENSSL_SOURCE_SHA256)
 
 message(STATUS "OpenSSL Source SHA256: " ${OPENSSL_SOURCE_SHA256})
 message(STATUS "Expected OpenSSL Source SHA256: " ${OPENSSL_SOURCE_SHA256_EXPECTED})
@@ -36,13 +39,18 @@ endif()
 
 if(NOT EXISTS ${SSL_ARCHIVE_PATH})
 	execute_process(
-		COMMAND build_all.cmd openssl-1.1.0h
-		WORKING_DIRECTORY "${SSL_PATH}/Windows"
+		COMMAND build_all.cmd openssl-${OPENSSL_SOURCE_VER}
+		WORKING_DIRECTORY "${SSL_WORK_DIR}"
+		OUTPUT_FILE "${SSL_WORK_DIR}/build_output.txt"
 		)
 endif()
 
 if(NOT EXISTS ${SSL_ARCHIVE_PATH})
 	message(FATAL_ERROR "INTEL SGX SSL Build failed!!")
+endif()
+
+if(NOT EXISTS ${SSL_BUILD_DIR})
+	file(MAKE_DIRECTORY ${SSL_BUILD_DIR})
 endif()
 
 execute_process(
