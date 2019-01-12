a3d6438dfb1525690fa97d226a3694703ad00060
zhenghaven <hazheng@mix.wvu.edu>
2019-01-11 17:08:43 -0800

    Added initial structure for whitelist.

---
+++ sources/common/WhiteList/HardCoded.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "WhiteList.h"
+
+namespace Decent
+{
+	namespace WhiteList
+	{
+		namespace HardCoded
+		{
+			static const WhiteListType& Get();
+		}
+	}
+}
---
+++ sources/common/WhiteList/WhiteList.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <map>
+
+namespace Decent
+{
+	namespace WhiteList
+	{
+		typedef std::map<std::string, std::string> WhiteListType;
+	}
+}
