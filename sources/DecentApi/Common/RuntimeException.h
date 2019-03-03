e9f866926fd79328b9c458722c5e7c42d3d69b55
zhenghaven <hazheng@mix.wvu.edu>
2019-03-02 18:09:03 -0800

    Fixed bugs for Linux build.

--- sources/DecentApi/Common/RuntimeException.h
+++ sources/DecentApi/Common/RuntimeException.h
@@ -1,19 +1,19 @@
 #pragma once
 
-#include <exception>
+#include <stdexcept>
 #include <string>
 
 namespace Decent
 {
-	class RuntimeException : public std::exception
+	class RuntimeException : public std::runtime_error
 	{
 	public:
 		explicit RuntimeException(const std::string& what_arg) :
-			std::exception(what_arg.c_str())
+			std::runtime_error(what_arg.c_str())
 		{}
 
 		explicit RuntimeException(const char* what_arg) :
-			std::exception(what_arg)
+			std::runtime_error(what_arg)
 		{}
 
 	private:
