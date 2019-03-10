59ee94333e32c0defacb6fc6632d61d0f169c342
zhenghaven <hazheng@mix.wvu.edu>
2019-03-10 01:51:58 -0800

    Corrected the name error in OcallConnection.

---
+++ sources/DecentApi/CommonEnclave/SGX/OcallConnector.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "edl_decent_net.h"
+#include "../../Common/Net/NetworkException.h"
+
+namespace Decent
+{
+	namespace Net
+	{
+		/** \brief	An OCall connector that uses OCall function to establish connections. */
+		struct OcallConnector
+		{
+			/**
+			 * \brief	Constructor
+			 *
+			 * \exception	Decent::Net::Exception	Thrown when an exception error condition occurs. Error
+			 * 										conditions are OCall failed, or the connection pointer
+			 * 										received is null.
+			 *
+			 * \tparam	FuncT	Type of the OCall function, which must has the forms of "sgx_status_t
+			 * 					FuncName(void* connection_ptr, ...)", where the "..." means any number of
+			 * 					parameters, not the variable argument list.
+			 * \tparam	Args 	Type of the arguments.
+			 * \param	cntBuilder	The OCall function.
+			 * \param	args	  	Variable arguments providing [in,out] The arguments.
+			 */
+			template<class FuncT, class... Args>
+			OcallConnector(FuncT cntBuilder, Args&&... args) :
+				m_ptr(nullptr)
+			{
+				if ((*cntBuilder)(&m_ptr, std::forward<Args>(args)...) != SGX_SUCCESS ||
+					m_ptr == nullptr)
+				{
+					throw Decent::Net::Exception("Failed to establish connection via OCALL.");
+				}
+			}
+
+			~OcallConnector()
+			{
+				ocall_decent_net_cnet_close(m_ptr);
+			}
+			void* m_ptr;
+		};
+	}
+}
