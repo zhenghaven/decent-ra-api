97d626457718dbc5ca50c96dc8cc2d95a325de90
zhenghaven <hazheng@mix.wvu.edu>
2019-04-09 18:12:29 -0700

    Simplified DownloadSgxSdk.cmake

--- cmake/DownloadSgxSdk.cmake
+++ cmake/DownloadSgxSdk.cmake
@@ -3,6 +3,110 @@ cmake_minimum_required(VERSION 3.0)
 message(STATUS "")
 message(STATUS "Checking Intel SGX SDK...")
 
+FUNCTION(Download_Package_And_Check SourceUrl DestPath ExpectedHash)
+	file(DOWNLOAD
+		${SourceUrl}
+		${DestPath}
+		#SHOW_PROGRESS
+	)
+	
+	file(SHA256 ${DestPath} DOWNLOADED_SHA256)
+	
+	if(NOT ${DOWNLOADED_SHA256} STREQUAL ${ExpectedHash})
+		message(FATAL_ERROR "The checksum of downloaded file, ${DestPath}, is unexpected!")
+	endif()
+ENDFUNCTION()
+
+FUNCTION(Install_Checksum_Linux TmpFilePath CheckSum InstallationDirPath)
+	file(WRITE ${TmpFilePath} "${CheckSum}")
+	
+	execute_process(
+		RESULT_VARIABLE exeProcRetVal
+		ERROR_VARIABLE  exeProcRetErr
+		COMMAND sudo mv "${TmpFilePath}" "./SHA256"
+		WORKING_DIRECTORY "${InstallationDirPath}"
+		)
+	if(NOT ${exeProcRetVal} STREQUAL "0")
+		message(FATAL_ERROR "Failed to install the checksum in ${InstallationDirPath}. (Err: ${exeProcRetErr})")
+	endif()
+ENDFUNCTION()
+
+FUNCTION(Uninsall_Checksum_Linux ParentPath)
+	if(EXISTS "${ParentPath}/SHA256")
+		execute_process(
+			RESULT_VARIABLE exeProcRetVal
+			ERROR_VARIABLE  exeProcRetErr
+			COMMAND sudo rm -f "${ParentPath}/SHA256"
+			WORKING_DIRECTORY "${ParentPath}"
+			)
+		if(NOT ${exeProcRetVal} STREQUAL "0")
+				message(FATAL_ERROR "Failed to uninstall the checksum for ${ParentPath}. (Err: ${exeProcRetErr})")
+		endif()
+	endif()
+ENDFUNCTION()
+
+FUNCTION(Cleanup_Installation_Linux DirPath ScriptPath)
+	if(EXISTS "${DirPath}/${ScriptPath}")
+		execute_process(
+			RESULT_VARIABLE exeProcRetVal
+			ERROR_VARIABLE  exeProcRetErr
+			COMMAND sudo "./${ScriptPath}"
+			WORKING_DIRECTORY "${DirPath}"
+			)
+		if(NOT ${exeProcRetVal} STREQUAL "0")
+				message(FATAL_ERROR "Failed to cleanup the directory, ${DirPath}. (Err: ${exeProcRetErr})")
+		endif()
+	endif()
+ENDFUNCTION()
+
+FUNCTION(Uninstall_SGX_PSW_Linux)
+	if(EXISTS "${INTEL_SGX_PSW_INSTALL_DIR}")
+		execute_process(
+			RESULT_VARIABLE exeProcRetVal
+			ERROR_VARIABLE  exeProcRetErr
+			COMMAND sudo apt --assume-yes remove libsgx-enclave-common
+			WORKING_DIRECTORY "${INTEL_SGX_PSW_INSTALL_DIR}"
+			)
+		if(NOT ${exeProcRetVal} STREQUAL "0")
+				message(FATAL_ERROR "Failed to uninstall the existing SGX PSW. (Err: ${exeProcRetErr})")
+		endif()
+		
+		Cleanup_Installation_Linux(${INTEL_SGX_PSW_INSTALL_DIR} "cleanup.sh")
+		Cleanup_Installation_Linux(${INTEL_SGX_PSW_INSTALL_DIR} "uninstall.sh")
+		
+		Uninsall_Checksum_Linux(${INTEL_SGX_PSW_INSTALL_DIR})
+	endif()
+ENDFUNCTION()
+
+FUNCTION(Uninstall_SGX_Driver_Manual_Linux)
+	if(EXISTS "${INTEL_SGX_DRI_INSTALL_DIR}/isgx.ko")
+		
+		Uninstall_SGX_PSW_Linux()
+		
+		execute_process(
+			RESULT_VARIABLE exeProcRetVal
+			ERROR_VARIABLE  exeProcRetErr
+			#COMMAND sudo service aesmd stop
+			COMMAND sudo /sbin/modprobe -r isgx
+			COMMAND sudo rm -rf ${INTEL_SGX_DRI_INSTALL_DIR}
+			COMMAND sudo /sbin/depmod
+			COMMAND sudo /bin/sed -i '/^isgx$/d' /etc/modules
+			WORKING_DIRECTORY "${INTEL_SGX_DRI_INSTALL_DIR}"
+			)
+		if(NOT ${exeProcRetVal} STREQUAL "0")
+			message(FATAL_ERROR "Failed to uninstall the existing SGX driver. (Err: ${exeProcRetErr})")
+		endif()
+	endif()
+ENDFUNCTION()
+
+FUNCTION(Uninstall_SGX_Driver_Script_Linux)
+	
+	Uninstall_SGX_PSW_Linux()
+	
+	Cleanup_Installation_Linux("${INTEL_SGX_INSTALL_DIR}/sgxdriver" "uninstall.sh")
+	
+ENDFUNCTION()
+
 if(WIN32)
 
 	set(INTEL_SGX_SDK_BIN_URL "")
@@ -44,7 +148,7 @@ elseif(UNIX AND NOT APPLE)
 		######
 		set(READ_INSTALLED_SHA256 "N/A")
 		if(EXISTS "${INTEL_SGX_SDK_INSTALL_DIR}/SHA256")
-		 file(READ ${INTEL_SGX_SDK_INSTALL_DIR}/SHA256 READ_INSTALLED_SHA256)
+			file(READ ${INTEL_SGX_SDK_INSTALL_DIR}/SHA256 READ_INSTALLED_SHA256)
 		endif()
 		
 		message(STATUS "SDK Ver installed: ${READ_INSTALLED_SHA256}")
@@ -53,29 +157,11 @@ elseif(UNIX AND NOT APPLE)
 		
 			message(STATUS "Couldn't find the SDK we need, try to install one...")
 			
-			file(DOWNLOAD 
-			${INTEL_SGX_SDK_BIN_URL}  
-			${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin/sgx_linux_x64_sdk.bin 
-			SHOW_PROGRESS
-			)
+			set(INTEL_SGX_SDK_BIN_DOWNLOADED_PATH ${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin/sgx_linux_x64_sdk.bin)
 			
-			file(SHA256 ${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin/sgx_linux_x64_sdk.bin DOWNLOADED_SHA256)
+			Download_Package_And_Check(${INTEL_SGX_SDK_BIN_URL} ${INTEL_SGX_SDK_BIN_DOWNLOADED_PATH} ${INTEL_SGX_SDK_BIN_SHA256})
 			
-			if(NOT ${DOWNLOADED_SHA256} STREQUAL ${INTEL_SGX_SDK_BIN_SHA256})
-				message(FATAL_ERROR "The checksum of downloaded file is invalid!")
-			endif()
-			
-			if(EXISTS "${INTEL_SGX_SDK_INSTALL_DIR}/uninstall.sh")
-				execute_process(
-					RESULT_VARIABLE exeProcRetVal
-					ERROR_VARIABLE  exeProcRetErr
-					COMMAND sudo ./uninstall.sh
-					WORKING_DIRECTORY "${INTEL_SGX_SDK_INSTALL_DIR}"
-					)
-				if(NOT ${exeProcRetVal} STREQUAL "0")
-					message(FATAL_ERROR "Failed to uninstall the existing SGX SDK. (Err: ${exeProcRetErr})")
-				endif()
-			endif()
+			Cleanup_Installation_Linux(${INTEL_SGX_SDK_INSTALL_DIR} "uninstall.sh")
 			
 			execute_process(
 				RESULT_VARIABLE exeProcRetVal
@@ -88,17 +174,9 @@ elseif(UNIX AND NOT APPLE)
 				message(FATAL_ERROR "Failed to install the SGX SDK. (Err: ${exeProcRetErr})")
 			endif()
 			
-			file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin/SDK/SHA256 "${INTEL_SGX_SDK_BIN_SHA256}")
+			set(INTEL_SGX_SDK_BIN_HASH_TMP_PATH ${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin/SDK/SHA256)
 			
-			execute_process(
-				RESULT_VARIABLE exeProcRetVal
-				ERROR_VARIABLE  exeProcRetErr
-				COMMAND sudo mv "./SDK/SHA256" "${INTEL_SGX_SDK_INSTALL_DIR}/SHA256"
-				WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin"
-				)
-			if(NOT ${exeProcRetVal} STREQUAL "0")
-				message(FATAL_ERROR "Failed to install the checksum for SGX SDK. (Err: ${exeProcRetErr})")
-			endif()
+			Install_Checksum_Linux(${INTEL_SGX_SDK_BIN_HASH_TMP_PATH} ${INTEL_SGX_SDK_BIN_SHA256} ${INTEL_SGX_SDK_INSTALL_DIR})
 			
 			message(STATUS "Successfully installed Intel SGX SDK!")
 			
@@ -111,7 +189,7 @@ elseif(UNIX AND NOT APPLE)
 			######
 			set(READ_INSTALLED_SHA256 "N/A")
 			if(EXISTS "${INTEL_SGX_DRI_INSTALL_DIR}/SHA256")
-			 file(READ ${INTEL_SGX_DRI_INSTALL_DIR}/SHA256 READ_INSTALLED_SHA256)
+				file(READ ${INTEL_SGX_DRI_INSTALL_DIR}/SHA256 READ_INSTALLED_SHA256)
 			endif()
 			
 			message(STATUS "Driver Ver installed: ${READ_INSTALLED_SHA256}")
@@ -120,42 +198,14 @@ elseif(UNIX AND NOT APPLE)
 			
 				message(STATUS "Couldn't find the driver we need, try to install one...")
 				
-				file(DOWNLOAD 
-				${INTEL_SGX_DRI_BIN_URL}  
-				${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin/sgx_linux_x64_driver.bin 
-				SHOW_PROGRESS
-				)
+				set(INTEL_SGX_DRI_BIN_DOWNLOADED_PATH ${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin/sgx_linux_x64_driver.bin)
 				
-				file(SHA256 ${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin/sgx_linux_x64_driver.bin DOWNLOADED_SHA256)
+				Download_Package_And_Check(${INTEL_SGX_DRI_BIN_URL} ${INTEL_SGX_DRI_BIN_DOWNLOADED_PATH} ${INTEL_SGX_DRI_BIN_SHA256})
 				
-				if(NOT ${DOWNLOADED_SHA256} STREQUAL ${INTEL_SGX_DRI_BIN_SHA256})
-					message(FATAL_ERROR "The checksum of downloaded file is invalid!")
-				endif()
+				## The uninstallation script came from the package is buggy.
+				#Uninstall_SGX_Driver_Script_Linux()
 				
-				if(EXISTS "${INTEL_SGX_DRI_INSTALL_DIR}/isgx.ko")
-					if(EXISTS "${INTEL_SGX_INSTALL_DIR}/sgxdriver/uninstall.sh")
-						execute_process(
-							RESULT_VARIABLE exeProcRetVal
-							ERROR_VARIABLE  exeProcRetErr
-							COMMAND sudo ./uninstall.sh
-							WORKING_DIRECTORY "${INTEL_SGX_INSTALL_DIR}/sgxdriver/"
-							)
-					else()
-						execute_process(
-							RESULT_VARIABLE exeProcRetVal
-							ERROR_VARIABLE  exeProcRetErr
-							COMMAND sudo service aesmd stop
-							COMMAND sudo /sbin/modprobe -r isgx
-							COMMAND sudo rm -rf ${INTEL_SGX_DRI_INSTALL_DIR}
-							COMMAND sudo /sbin/depmod
-							COMMAND sudo /bin/sed -i '/^isgx$/d' /etc/modules
-							WORKING_DIRECTORY "${INTEL_SGX_DRI_INSTALL_DIR}"
-							)
-					endif()
-					if(NOT ${exeProcRetVal} STREQUAL "0")
-						message(FATAL_ERROR "Failed to uninstall the existing SGX driver. (Err: ${exeProcRetErr})")
-					endif()
-				endif()
+				Uninstall_SGX_Driver_Manual_Linux()
 				
 				execute_process(
 					RESULT_VARIABLE exeProcRetVal
@@ -168,17 +218,9 @@ elseif(UNIX AND NOT APPLE)
 					message(FATAL_ERROR "Failed to install the SGX driver. (Err: ${exeProcRetErr})")
 				endif()
 				
-				file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin/Driver/SHA256 "${INTEL_SGX_DRI_BIN_SHA256}")
+				set(INTEL_SGX_DRI_BIN_HASH_TMP_PATH ${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin/SDK/SHA256)
 				
-				execute_process(
-					RESULT_VARIABLE exeProcRetVal
-					ERROR_VARIABLE  exeProcRetErr
-					COMMAND sudo mv "./Driver/SHA256" "${INTEL_SGX_DRI_INSTALL_DIR}/SHA256"
-					WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin"
-					)
-				if(NOT ${exeProcRetVal} STREQUAL "0")
-					message(FATAL_ERROR "Failed to install the checksum for SGX driver. (Err: ${exeProcRetErr})")
-				endif()
+				Install_Checksum_Linux(${INTEL_SGX_DRI_BIN_HASH_TMP_PATH} ${INTEL_SGX_DRI_BIN_SHA256} ${INTEL_SGX_DRI_INSTALL_DIR})
 				
 				message(STATUS "Successfully installed Intel SGX Driver!")
 				
@@ -189,7 +231,7 @@ elseif(UNIX AND NOT APPLE)
 			######
 			set(READ_INSTALLED_SHA256 "N/A")
 			if(EXISTS "${INTEL_SGX_PSW_INSTALL_DIR}/SHA256")
-			 file(READ ${INTEL_SGX_PSW_INSTALL_DIR}/SHA256 READ_INSTALLED_SHA256)
+				file(READ ${INTEL_SGX_PSW_INSTALL_DIR}/SHA256 READ_INSTALLED_SHA256)
 			endif()
 			
 			message(STATUS "PSW Ver installed: ${READ_INSTALLED_SHA256}")
@@ -199,53 +241,26 @@ elseif(UNIX AND NOT APPLE)
 			
 				message(STATUS "Couldn't find the PSW we need, try to install one...")
 				
-				file(DOWNLOAD 
-				${INTEL_SGX_PSW_BIN_URL}  
-				${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin/sgx_linux_x64_psw.deb 
-				SHOW_PROGRESS
-				)
+				set(INTEL_SGX_PSW_BIN_DOWNLOADED_PATH ${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin/sgx_linux_x64_psw.deb)
 				
-				file(SHA256 ${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin/sgx_linux_x64_psw.deb DOWNLOADED_SHA256)
+				Download_Package_And_Check(${INTEL_SGX_PSW_BIN_URL} ${INTEL_SGX_PSW_BIN_DOWNLOADED_PATH} ${INTEL_SGX_PSW_BIN_SHA256})
 				
-				if(NOT ${DOWNLOADED_SHA256} STREQUAL ${INTEL_SGX_PSW_BIN_SHA256})
-					message(FATAL_ERROR "The checksum of downloaded file is invalid!")
-				endif()
-				
-				if(EXISTS "${INTEL_SGX_PSW_INSTALL_DIR}/uninstall.sh")
-					execute_process(
-						RESULT_VARIABLE exeProcRetVal
-						ERROR_VARIABLE  exeProcRetErr
-						COMMAND sudo ./uninstall.sh
-						COMMAND sudo apt --assume-yes remove libsgx-enclave-common
-						WORKING_DIRECTORY "${INTEL_SGX_PSW_INSTALL_DIR}"
-						)
-					if(NOT ${exeProcRetVal} STREQUAL "0")
-							message(FATAL_ERROR "Failed to uninstall the existing SGX PSW. (Err: ${exeProcRetErr})")
-					endif()
-				endif()
+				Uninstall_SGX_PSW_Linux()
 				
 				execute_process(
 					RESULT_VARIABLE exeProcRetVal
 					ERROR_VARIABLE  exeProcRetErr
 					COMMAND chmod +x ./sgx_linux_x64_psw.deb
-					COMMAND sudo apt --assume-yes install ${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin/sgx_linux_x64_psw.deb
+					COMMAND sudo apt --assume-yes install ${INTEL_SGX_PSW_BIN_DOWNLOADED_PATH}
 					WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin"
 					)
 				if(NOT ${exeProcRetVal} STREQUAL "0")
 					message(FATAL_ERROR "Failed to install the SGX PSW. (Err: ${exeProcRetErr})")
 				endif()
 				
-				file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin/PSW/SHA256 "${INTEL_SGX_PSW_BIN_SHA256}")
+				set(INTEL_SGX_PSW_BIN_HASH_TMP_PATH ${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin/SDK/SHA256)
 				
-				execute_process(
-					RESULT_VARIABLE exeProcRetVal
-					ERROR_VARIABLE  exeProcRetErr
-					COMMAND sudo mv "./PSW/SHA256" "${INTEL_SGX_PSW_INSTALL_DIR}/SHA256"
-					WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/Intel_SGX_Bin"
-					)
-				if(NOT ${exeProcRetVal} STREQUAL "0")
-					message(FATAL_ERROR "Failed to install the checksum for SGX PSW. (Err: ${exeProcRetErr})")
-				endif()
+				Install_Checksum_Linux(${INTEL_SGX_PSW_BIN_HASH_TMP_PATH} ${INTEL_SGX_PSW_BIN_SHA256} ${INTEL_SGX_PSW_INSTALL_DIR})
 				
 				message(STATUS "Successfully installed Intel SGX PSW!")
 				
