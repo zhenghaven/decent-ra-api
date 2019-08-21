c45cdfc6165b508e9eddc73cd26a9fcb12d2b60f
Haofan Zheng <hazheng@mix.wvu.edu>
2019-08-20 22:37:07 -0700

    Added Drbg random generator for supporting STL distribution funcs.

--- sources/DecentApi/Common/MbedTls/Drbg.h
+++ sources/DecentApi/Common/MbedTls/Drbg.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <cstddef>
+#include <limits>
 
 namespace Decent
 {
@@ -30,6 +30,39 @@ namespace Decent
 
 		private:
 			void* m_state;
+		};
+
+		template<typename ResultType>
+		class DrbgRandGenerator
+		{
+		public:
+			typedef ResultType result_type;
+
+			static constexpr ResultType max()
+			{
+				return std::numeric_limits<ResultType>::max();
+			}
+
+			static constexpr ResultType min()
+			{
+				return std::numeric_limits<ResultType>::min();
+			}
+
+		public:
+			DrbgRandGenerator() = default;
+
+			virtual ~DrbgRandGenerator()
+			{}
+
+			ResultType operator()()
+			{
+				ResultType res;
+				m_drbg.Rand(&res, sizeof(res));
+				return res;
+			}
+
+		private:
+			MbedTlsObj::Drbg m_drbg;
 		};
 	}
 }
