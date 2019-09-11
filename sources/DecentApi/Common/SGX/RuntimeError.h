622299382d894d70fe105d498a68909d046efcc7
zhenghaven <hazheng@mix.wvu.edu>
2019-09-10 18:42:02 -0700

    Added more macros to check SGX func call errors.

--- sources/DecentApi/Common/SGX/RuntimeError.h
+++ sources/DecentApi/Common/SGX/RuntimeError.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <type_traits>
+
 #include "../RuntimeException.h"
 
 #include "ErrorCode.h"
@@ -13,6 +15,54 @@
 
 #define DECENT_CHECK_SGX_FUNC_CALL_ERROR(FUNC, ...) \
             DECENT_CHECK_SGX_STATUS_ERROR(FUNC(__VA_ARGS__), FUNC)
+
+#define DECENT_SGX_CALL_WITH_INTBOOL_ERROR_1(FUNC) \
+	{\
+		int retVal = false; \
+		const auto sgxRet = FUNC(&retVal); \
+		if (sgxRet != SGX_SUCCESS) {\
+			throw Decent::Sgx::RuntimeError(sgxRet, #FUNC); \
+		} \
+		if (!retVal) { \
+			throw Decent::RuntimeException(#FUNC "return failed."); \
+		} \
+	}
+
+#define DECENT_SGX_CALL_WITH_INTBOOL_ERROR(FUNC, ...) \
+	{\
+		int retVal = false; \
+		const auto sgxRet = FUNC(&retVal, __VA_ARGS__); \
+		if (sgxRet != SGX_SUCCESS) {\
+			throw Decent::Sgx::RuntimeError(sgxRet, #FUNC); \
+		} \
+		if (!retVal) { \
+			throw Decent::RuntimeException(#FUNC "return failed."); \
+		} \
+	}
+
+#define DECENT_SGX_CALL_WITH_PTR_ERROR_1(FUNC, PTR) \
+	{\
+		static_assert(std::is_pointer<decltype(PTR)>::value, #PTR "must be a pointer."); \
+		const auto sgxRet = FUNC(&PTR); \
+		if (sgxRet != SGX_SUCCESS) { \
+			throw Decent::Sgx::RuntimeError(sgxRet, #FUNC); \
+		} \
+		if (PTR == nullptr) { \
+			throw Decent::RuntimeException(#FUNC "return failed."); \
+		} \
+	}
+
+#define DECENT_SGX_CALL_WITH_PTR_ERROR(FUNC, PTR, ...) \
+	{\
+		static_assert(std::is_pointer<decltype(PTR)>::value, #PTR "must be a pointer."); \
+		const auto sgxRet = FUNC(&PTR, __VA_ARGS__); \
+		if (sgxRet != SGX_SUCCESS) { \
+			throw Decent::Sgx::RuntimeError(sgxRet, #FUNC); \
+		} \
+		if (PTR == nullptr) { \
+			throw Decent::RuntimeException(#FUNC "return failed."); \
+		} \
+	}
 
 namespace Decent
 {
